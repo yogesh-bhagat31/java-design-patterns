d3cb6b494576f54c55bbde58f5b9cd12e9fe0525
yogesh-bhagat31 <yogesh.bhagat.b31@gmail.com>
2026-09-24 19:54:04 +0530

    add memento design pattern 2

---
+++ Design-Patterns/src/main/java/com/yogesh/lld/designpatterns/behavioral/memento/Caretaker.java
@@ -0,0 +1,25 @@
+package com.yogesh.lld.designpatterns.behavioral.memento;
+
+import java.util.Stack;
+
+public class Caretaker {
+
+    //Here we are just using memento as state, but we can not modify or see anything as state is private
+    private Stack<TextEditor.Memento> history = new Stack<>();
+
+    public void save(TextEditor editor) {
+        history.push(editor.save());
+        System.out.println("*** State saved ***");
+    }
+
+    public void undo(TextEditor editor) {
+        if (!history.isEmpty()) {
+            TextEditor.Memento memento = history.pop();
+            editor.restore(memento);
+            System.out.println("*** Undo performed ***");
+        } else {
+            System.out.println("*** Nothing to undo ***");
+        }
+    }
+
+}
